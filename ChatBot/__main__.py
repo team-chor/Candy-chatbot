8b45f2999ac3f6c4004b705db7bdd5653fcd3062
CodeSearchDev <150044321+CodeSearchDev@users.noreply.github.com>
2025-03-12 23:35:29 +0530

    Update Maim

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,9 +1,13 @@
+import os
 import importlib
+import subprocess
+import asyncio
 
 from pyrogram import idle
 
 from ChatBot import app
 from ChatBot.modules import ALL_MODULES
+from config import WEB_APP  
 
 async def boot():
     await app.start()
@@ -11,8 +15,12 @@ async def boot():
     for module in ALL_MODULES:
         importlib.import_module(f"ChatBot.modules.{module}")
 
+    if WEB_APP:
+        subprocess.Popen(['python3', 'app.py'])
+
     await idle()
     await app.stop()
 
 if __name__ == "__main__":
-    app.run(boot())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(boot())
