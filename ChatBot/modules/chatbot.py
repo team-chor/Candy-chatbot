dcbcaa139cd3fbea3afd945d7cdcca4569b03764
Sumi Loo <lannsnolo@gmail.com>
2025-05-07 17:40:48 +0545

    Update chatbot.py

--- ChatBot/modules/chatbot.py
+++ ChatBot/modules/chatbot.py
@@ -32,7 +32,7 @@ async def chatbot(_, message: Message):
 
     await app.send_chat_action(chat_id, ChatAction.TYPING)
     reply = chatbot_api.ask_question(message.text)
-    await message.reply_text(reply or "❖ ChatBot Error. Contact @ChatWithDevRobot.")
+    await message.reply_text(reply or "❖ ChatBot Error. Contact @TEAM_NETWORK_JJK.")
 
 @app.on_message(filters.command(["chatbot"]) & filters.group & ~filters.bot)
 @is_admins
